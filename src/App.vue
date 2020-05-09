0120564d9f3d04c9416f8eede7551f3f8d028cee
Vicky康康康 <1425988159@qq.com>
2020-05-09 18:19:16 +0800

    项目初始化-配置layout布局组件子路由

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <template>
   <div id="app">
-    <div>头条移动端</div>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <i class="iconfont iconshipin"></i>
-    <i class="iconfont icondianzan"></i>
     <router-view/>
   </div>
 </template>
