88b0f52c6015f9c32e8d094f059c662625e70492
Vicky康康康 <1425988159@qq.com>
2020-05-09 10:11:31 +0800

    项目初始化调整目录结构

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-
+// 创建Vue根实例,将router、store配置到根实例中
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app') // App根组件渲染到#app节点
