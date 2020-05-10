a414aeafa94e031776b0c5640012381c2a81fb78
Vicky康康康 <1425988159@qq.com>
2020-05-10 09:42:37 +0800

    调整全局样式加载的顺序

--- src/main.js
+++ src/main.js
@@ -2,13 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './styles/index.less'
+
 // 导入vant组件库
 import Vant from 'vant'
+// 导入vant的样式
 import 'vant/lib/index.css'
 
 // 这个插件可以自动设置rem基准值
 import 'amfe-flexible'
+
+// 导入自己全局的样式(放在后面的原因:自己的样式可覆盖第三方插件的样式)
+import './styles/index.less'
+
 // 注册全局使用组件
 Vue.use(Vant)
 
