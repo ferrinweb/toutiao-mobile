529bf66390595443058fd8f2019758e8432cb213
Vicky康康康 <1425988159@qq.com>
2020-05-19 22:27:25 +0800

    评论点赞功能完成:注意请求完成后的视图层数据的更新

--- src/api/comment.js
+++ src/api/comment.js
@@ -7,3 +7,20 @@ export const getComments = params => {
     params
   })
 }
+// 给评论点赞
+export const toLikeComment = commentId => {
+  return request({
+    method: 'POST',
+    url: '/app/v1_0/comment/likings',
+    data: {
+      target: commentId
+    }
+  })
+}
+// 取消评论点赞
+export const deleteCommentLike = commentId => {
+  return request({
+    method: 'DELETE',
+    url: `/app/v1_0/comment/likings/${commentId}`
+  })
+}
--- src/views/article/components/comment-item.vue
+++ src/views/article/components/comment-item.vue
@@ -13,8 +13,8 @@
         <div class="username-wrap">
           <span class="user-name">{{comment.aut_name}}</span>
           <!-- 点赞评论图标 -->
-          <span>
-            <van-icon name="good-job-o" />
+          <span @click="commentLikeOprate">
+            <van-icon :class="{like: comment.is_liking}" :name="comment.is_liking ? 'good-job' : 'good-job-o'" />
             <span class="like-count">{{comment.like_count}}</span>
           </span>
         </div>
@@ -35,12 +35,33 @@
 </template>
 
 <script>
+import { toLikeComment, deleteCommentLike } from '@/api/comment'
 export default {
   name: 'CommentItem',
   props: {
     comment: {
       type: Object,
       required: true
+    }
+  },
+  methods: {
+    // 点赞/取消点赞的函数
+    async commentLikeOprate () {
+      this.$toast.loading({
+        message: '点赞中...',
+        forbidClick: true
+      })
+      const commentId = this.comment.com_id.toString()
+      if (this.comment.is_liking) {
+        await deleteCommentLike(commentId)
+        this.comment.like_count--
+      } else {
+        await toLikeComment(commentId)
+        this.comment.like_count++
+      }
+      // 更新视图
+      this.comment.is_liking = !this.comment.is_liking
+      this.$toast.success(this.comment.is_liking ? '点赞成功' : '取消点赞成功')
     }
   }
 }
@@ -64,6 +85,10 @@ export default {
     }
     .like-count{
       font-size: 12px;
+      margin-left: 1px;
+    }
+    .like{
+      color: hotpink;
     }
   }
   // 评论正文部分
