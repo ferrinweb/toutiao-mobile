1828567e54a78dd94f2d288bcf9c32d8239b32fc
Vicky康康康 <1425988159@qq.com>
2020-05-21 18:01:59 +0800

    修改用户昵称页面布局

---
+++ src/views/user-profile/components/edit-username.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="edit-username">
+    <van-nav-bar
+      title="修改昵称"
+      left-text="取消"
+      right-text="完成"
+      @click-left="$emit('close')"
+      @click-right="onConfirm"
+    />
+     <div class="username">
+       <van-field
+        v-model="message"
+        rows="2"
+        autosize
+        type="textarea"
+        maxlength="7"
+        placeholder="请输入昵称"
+        show-word-limit
+      />
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditUsername',
+  data () {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    onConfirm () {
+      console.log('onConfirm')
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.edit-username{
+  .username{
+    padding: 10px;
+    .van-field{
+      height: 80px;
+    }
+  }
+}
+</style>
--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -14,21 +14,41 @@
       :src="user.photo"
     />
   </van-cell>
-  <van-cell title="昵称" :value="user.name" is-link />
+  <van-cell
+    title="昵称"
+    :value="user.name"
+    is-link
+    @click="isEditUsernameShow = true"
+  />
   <van-cell title="性别" :value="user.gender === 0 ? '男' : '女'" is-link />
   <van-cell title="生日" :value="user.birthday" is-link />
+  <!-- 用户名弹层 -->
+  <van-popup
+    v-model="isEditUsernameShow"
+    position="bottom"
+    :style="{ height: '100%' }"
+    class="edit-name-pop"
+  >
+  <!-- 编辑用户个人信息弹层 -->
+    <edit-username
+      @close="isEditUsernameShow = false"
+    />
+  </van-popup>
   </div>
 </template>
 
 <script>
+import EditUsername from './components/edit-username'
 import { getUserProfile } from '@/api/user'
 export default {
   name: 'UserProfile',
   data () {
     return {
-      user: {}
+      user: {}, // 用户个人信息
+      isEditUsernameShow: false
     }
   },
+  components: { EditUsername },
   created () {
     this.loaderUserProfile()
   },
@@ -47,6 +67,10 @@ export default {
   .user-avatar{
     width: 35px;
     height: 35px;
+  }
+  // 用户名修改弹层
+  .edit-name-pop{
+    background-color: #f5f7f9;
   }
 }
 </style>
